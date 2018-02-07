11aecb64d39f94323645b47e231cc8d4b1017a98
BLaurenB <blbillington1@gmail.com>
2018-02-07 09:22:25 -0700

    postman requests working, writing feature test

---
+++ spec/features/user_sees_stations_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "User can see closest stations" do
+  scenario "" do
+    VCR.user_casseette("closest_stations")
+
+
+      # As a user
+      # When I visit "/"
+      visit "/"
+      # And I fill in the search form with 80203
+      fill_in "Search by zip...", with "80203"
+      # And I click "Locate"
+      click_button "Locate"
+      # Then I should be on page "/search" with parameters visible in the url
+      expect(current_path).to be
+      # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+
+      # And the stations should be limited to Electric and Propane
+
+      # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+
+    end #end vcr
+  end
+end
