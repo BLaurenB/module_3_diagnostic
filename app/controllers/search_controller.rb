f27f8a0fead880d4db93a0913b777e1857f1d804
BLaurenB <blbillington1@gmail.com>
2018-02-07 10:50:03 -0700

    having trouble with faraday connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,16 @@
 class SearchController < ApplicationController
 
   def index
-  end 
+    # @stations = Station.method_to_call_api(params[:q])
+    @stations = JSON.parse(response.fuel_stations, symbolize_names: true)
 
+    connection = Faraday.new("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?&location=80203") do |f|
+      f.headers['X-Api-Key'] = ENV['API_USER_TOKEN']
+      f.adapter Faraday.default_adapter
+    end
 
-end
+      response = connection.get("&radius=6.0&fuel_type=ELEC,LPG&limit=10")
+
+    end
+
+  end
