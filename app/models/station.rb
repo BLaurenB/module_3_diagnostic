097b7c3f787bab849abdf742280448d7d2fae26b
BLaurenB <blbillington1@gmail.com>
2018-02-07 11:11:27 -0700

    incomplete, unable to get hashes turned into objects to send to the view

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,14 +1,21 @@
 class Station
+  attr_reader :name, :address, :fuel_type, :distance, :access_time
 
   def initialize(station)
-    @name = station[:name]
-    @address = station[:address]
-    @fuel_type = station[:fuel_type]
+    @name = station[:id]
+    @street_address = station[:street_address]
+    # @address = station[:address]
+    # @address = station[:address]
+    # @address = station[:address]
+    @fuel_type = station[:fuel_type_code]
     @distance = station[:distance]
-    @access_time = station[:access_time]
+    @access_time = station[:access_days_time]
   end
 
-  
+def address
+  #Need to decorate for the full adddress
+end
+
 
 
 end
